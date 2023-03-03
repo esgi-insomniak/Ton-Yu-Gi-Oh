738411450b6cc7602a71d0852fe88606e4e10269
Loan CLERIS <loan.cleris@gmail.com>
2023-03-03 11:08:02 +0100

    refacto micro-service

--- server/user-api/src/auth/auth.module.ts
+++ server/user-api/src/auth/auth.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
+import { LoginModule } from './login/login.module';
 import { LoginService } from './login/login.service';
 import { RegisterModule } from './register/register.module';
 import { RegisterService } from './register/register.service';
 
 @Module({
-    providers: [
-        LoginService,
-        RegisterService,
-    ],
-    imports: [RegisterModule],
+    imports: [RegisterModule, LoginModule],
 })
 export class AuthModule { }
